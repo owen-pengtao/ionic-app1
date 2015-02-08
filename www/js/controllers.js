6fce53a740d52c99489ebf5df5f284f0806ccaf8
Owen Peng <tpeng@tibco-support.com>
2015-02-07 22:19:11 -0800

    rename friends to my cards

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -70,12 +70,12 @@ angular.module('starter.controllers', ['starter.services'])
     };
   })
 
-  .controller('FriendsCtrl', function ($scope, Friends) {
-    $scope.friends = Friends.all();
+  .controller('MyCardsCtrl', function ($scope, MyCards) {
+    $scope.myCards = MyCards.all();
   })
 
-  .controller('FriendDetailCtrl', function ($scope, $stateParams, Friends) {
-    $scope.friend = Friends.get($stateParams.friendId);
+  .controller('MyCardDetailCtrl', function ($scope, $stateParams, MyCards) {
+    $scope.myCard = MyCards.get($stateParams.myCardId);
   })
 
   .controller('AccountCtrl', function ($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -60,51 +60,46 @@ angular.module('starter.services', [])
     add: function(card) {
       console.log(card);
     }
-  }
+  };
 })
 
 /**
  * A simple example service that returns some data.
  */
-.factory('Friends', function() {
+.factory('MyCards', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var friends = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    notes: 'Enjoys drawing things',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  var myCards = [{
+    id: 1,
+    aliasName   : "Chase Freedom",
+    bankID      : 1,
+    bankName    : "Chase",
+    cardType    : "Credit",
+    validDate   : "08/2019",
+    point       : "2000",
+    creditLimit : "5000",
+    createTime  : "02/07/2015"
   }, {
     id: 1,
-    name: 'Max Lynx',
-    notes: 'Odd obsession with everything',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlen',
-    notes: 'Wears a sweet leather Jacket. I\'m a bit jealous',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    notes: 'I think he needs to buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    notes: 'Just the nicest guy',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    aliasName   : "AMEX Costco",
+    bankID      : 5,
+    bankName    : "American Express",
+    cardType    : "Credit",
+    validDate   : "08/2017",
+    point       : "3000",
+    creditLimit : "2000",
+    createTime  : "02/06/2015"
   }];
 
 
   return {
     all: function() {
-      return friends;
+      return myCards;
     },
-    get: function(friendId) {
+    get: function(myCardId) {
       // Simple index lookup
-      return friends[friendId];
+      return myCards[myCardId];
     }
-  }
+  };
 });
